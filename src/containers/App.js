fb94dcfed0842097bce9aa31d87f8769230e0860
github username <github email address>
2019-03-13 11:46:44 -0400

    Added profiles route and home component

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Route } from 'react-router-dom';
 import './App.css';
+import Home from '../components/Home'
+import Profiles from './Profiles'
 
 class App extends Component {
   render() {
     return (
       <div>
         <nav>
+          <Link to="/">Home</Link>
           <Link to="/profiles">Profiles</Link>
         </nav>
+        <div>
+          <Route exact path="/" component={Home} />
+          <Route path="/profiles" component={Profiles} />
+        </div>
       </div>
     )
   }
