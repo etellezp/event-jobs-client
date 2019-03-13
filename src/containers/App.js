5c0f3feae61d891f5e99a5cf808db4fa46c0182a
github username <github email address>
2019-03-13 11:27:31 -0400

    Added react router and link to profiles

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div>
-        Hello World
+        <nav>
+          <Link to="/profiles">Profiles</Link>
+        </nav>
       </div>
     )
   }
