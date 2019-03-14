e8c4faf2e2db9fedc7a969ecc2751ee2971d5006
github username <github email address>
2019-03-14 10:49:40 -0400

    Succesfully added a form that creates a profile and send it to the data base via fetch post request

--- src/components/ProfileForm.js
+++ src/components/ProfileForm.js
@@ -2,23 +2,63 @@ import React, { Component } from 'react';
 
 class ProfileForm extends Component {
 
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      name: '',
+      image_url: '',
+      about: '',
+      skill: '',
+      location: '',
+      rate: ''
+    }
+  }
+
+  handleOnSubmit = (event) => {
+    event.preventDefault()
+
+    const profileData = Object.assign({}, this.state)
+    const request = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        profile: profileData
+      })
+    }
+
+    fetch('http://localhost:3001/api/profiles ', request)
+      .then(response => response.json())
+      .then(profile => console.log(profile))
+      .catch(error => console.log(error))
+  }
+
+  handleOnChange = (event) => {
+    const { name, value } = event.target
+    this.setState({
+      [name]: value
+    })
+  }
+
   render() {
     return (
       <div>
         <h3>Add a Profile</h3>
-        <form>
+        <form onSubmit={this.handleOnSubmit}>
           <label htmlFor="name">Name</label>
-          <input type="text" name="name" />
+          <input onChange={this.handleOnChange} type="text" name="name" />
           <label htmlFor="image_url">Image</label>
-          <input type="text" name="image_url" />
+          <input onChange={this.handleOnChange} type="text" name="image_url" />
           <label htmlFor="about">About Me</label>
-          <textarea name="about"></textarea>
+          <textarea onChange={this.handleOnChange} name="about"></textarea>
           <label htmlFor="skill">Skill</label>
-          <input type="text" name="skill" />
+          <input onChange={this.handleOnChange} type="text" name="skill" />
           <label htmlFor="location">Location</label>
-          <input type="text" name="location" />
+          <input onChange={this.handleOnChange} type="text" name="location" />
           <label htmlFor="rate">Hourly Rate</label>
-          <input type="number" name="rate" />
+          <input onChange={this.handleOnChange} type="number" name="rate" />
           <button type="submit">Add Profile</button>
         </form>
       </div>
