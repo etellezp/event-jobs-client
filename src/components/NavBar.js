c8b10dc45b851eb6b97fb48bbf4b91a1e1403ed8
github username <github email address>
2019-04-09 13:46:00 -0400

    Added navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,54 @@
 import React from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink
+} from 'reactstrap';
 
-const NavBar = () => {
-  return (
-    <>
-      Navbar goes here
-    </>
-  )
+class NavBar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Navbar className="bg-dark navbar-dark" expand="md">
+          <NavbarBrand href="/">Uncublicle</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink className="text-white" href="/profiles">Profiles</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink className="text-white" href="#">Login</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink className="text-white" href="#">Register</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink className="text-white" href="#">LogOut</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
 }
 
 export default NavBar;
--- src/containers/App.js
+++ src/containers/App.js
@@ -9,14 +9,14 @@ import ProfileForm from '../components/ProfileForm';
 class App extends Component {
   render() {
     return (
-      <div>
+      <>
         <NavBar />
         <div>
           <Route exact path="/" component={Home} />
           <Route exact path="/profiles/new" component={ProfileForm} />
           <Route exact path="/profiles" component={Profiles} />
         </div>
-      </div>
+      </>
     )
   }
 }
