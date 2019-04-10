7dc4a8ba45031f2b5b91d09c21f719a1f7e4faf8
github username <github email address>
2019-04-10 12:47:15 -0400

    Work on the homepage styling and navbar background color

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Button } from 'reactstrap';
-
 
 const Home = () => (
-  <div className="container-fluid">
-    <h2>Hello World</h2>
-    <Button outline color="primary"><Link to="/profiles">Check Profiles</Link></Button>
+  <div className="container-fluid vh-100 m-0 p-0">
+    <div className="row align-items-center text-center">
+      <div className="col align-self-center">
+        <h1 className="display-1 font-weight-bold text-white my-5">Uncubicle</h1>
+        <h4 className="text-white font-weight-light w-50 mx-auto mb-5">
+          Welcome to Uncublicle, the platform where you can find great talent for your next big event.
+        </h4>
+        <button type="button" className="btn btn-primary p-2 mr-3"><Link to="/profiles">Check Profiles</Link></button>
+        <button type="button" className="btn btn-primary p-2 ml-3">I want to Work</button>
+      </div>
+    </div>
   </div>
 
 )
