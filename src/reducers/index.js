24ffd178fbed32cc0d4f7d71f36128f34f35453b
github username <github email address>
2019-04-26 14:41:06 -0400

    added comments reducer file, added to rootReducer, and added fetch comments reducer action

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import profilesReducer from './profilesReducer';
 import profileReducer from './profileReducer';
+import commentsReducer from './commentsReducer';
 
 const rootReducer = combineReducers({
   profiles: profilesReducer,
-  profile: profileReducer
+  profile: profileReducer,
+  comments: commentsReducer
 })
 
 export default rootReducer
